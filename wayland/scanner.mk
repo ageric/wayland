d14d2ecb20e974067033a57a13bfff8fae90c276
Michael Steinert <mike.steinert@gmail.com>
2010-12-01 17:18:43 -0700

    Keep generated sources with other source files
    Signed-off-by: Michael Steinert <mike.steinert@gmail.com>

--- wayland/scanner.mk
+++ wayland/scanner.mk
@@ -1,8 +1,8 @@
 %-protocol.c: $(top_srcdir)/protocol/%.xml
-	$(AM_V_GEN)$(wayland_scanner) code < $< > $@
+	$(AM_V_GEN)$(wayland_scanner) code < $< > $(top_srcdir)/wayland/$@
 
 %-server-protocol.h: $(top_srcdir)/protocol/%.xml
-	$(AM_V_GEN)$(wayland_scanner) server-header < $< > $@
+	$(AM_V_GEN)$(wayland_scanner) server-header < $< > $(top_srcdir)/wayland/$@
 
 %-client-protocol.h: $(top_srcdir)/protocol/%.xml
-	$(AM_V_GEN)$(wayland_scanner) client-header < $< > $@
+	$(AM_V_GEN)$(wayland_scanner) client-header < $< > $(top_srcdir)/wayland/$@
